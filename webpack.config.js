66a998dd44499a0fb32fd304a1cb842d594a5c72
Prateek Joshi <lastprateek@gmail.com>
2023-07-09 13:17:05 -0400

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
     mode: 'development',
@@ -56,7 +55,6 @@ module.exports = {
             title: 'Webpack App',
             filename: 'index.html',
             template: 'src/template.html'
-        }),
-        new BundleAnalyzerPlugin()
+        })
     ]
 };
